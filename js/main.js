435d833cb42937e34cfa2a7e1c4a9c6ecee25a19
ChrisAceto <caceto@gmu.edu>
2015-02-03 20:41:36 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ window.onload = function() {
         bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+        bouncy.anchor.setTo( 1, 0.5 );
         
         // Turn on the arcade physics engine for this sprite.
         game.physics.enable( bouncy, Phaser.Physics.ARCADE );
