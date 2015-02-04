a8ad2341347590f9e80ccbbc9f9311e671302616
ChrisAceto <caceto@gmu.edu>
2015-02-03 18:20:43 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ window.onload = function() {
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
         var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+        text.anchor.setTo( 1, 0.0 );
     }
     
     function update() {
