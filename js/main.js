d21afa727de9eea20ca36e219509a6126ff5eb49
narcher2 <narcher2@gmu.edu>
2015-02-05 05:52:20 -0500

    Everything

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ window.onload = function() {
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
+        var text = game.add.text( game.world.centerX, 15, "Everything.", style );
         text.anchor.setTo( 0.5, 0.0 );
     }
     
